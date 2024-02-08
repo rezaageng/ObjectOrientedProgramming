a861ab8bb071f817bef62240520379669366a7a3
rezaa <rezaageng5@gmail.com>
2024-02-08 17:53:40 +0700

    feat: initial commit

---
+++ src/Penjumlahan.java
@@ -0,0 +1,9 @@
+public class Penjumlahan {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 3;
+        int c = a + b;
+
+        System.out.println("Hasil penjumlahan: " + c);
+    }
+}
