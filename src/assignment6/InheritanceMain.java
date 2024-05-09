4b5f7004d65fd8836a4fa8ea81d0921e201e68da
rezaa <rezaageng5@gmail.com>
2024-05-09 17:02:25 +0700

    feat(assignment 6): add practice 1

---
+++ src/assignment6/InheritanceMain.java
@@ -0,0 +1,9 @@
+package assignment6;
+
+public class InheritanceMain {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.setNama("Reza Ageng Trihandoko");
+        System.out.println(mhs.getNama());
+    }
+}
---
+++ src/assignment6/Orang.java
@@ -0,0 +1,13 @@
+package assignment6;
+
+public class Orang {
+    protected String nama;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
